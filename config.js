7118f4a8eca5905c4a83477235ec15e06e81d6bc
ktd4869 <mztkn@vip.qq.com>
2022-02-07 16:13:04 +1100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,49 @@
+// ---------------------------------------------------------
+// Config
+// ---------------------------------------------------------
+
+var fieldName = {
+    "entity": "<ENTITY>",
+    "link": "<LINK>",
+    "id": "<ID>",
+    "date":"<DATE>",
+    "year":"<YEAR>",
+    "person":"<PERSON>",
+    "country":"<COUNTRY>",
+    "target":"<TARGET>",
+    "address":"<ADDRESS>",
+}
+var shortName = {
+    "entity": "ENTITY",
+    "link": "LINK",
+    "id": "ID",
+    "date":"DATE",
+    "year":"YEAR",
+    "person":"PERSON",
+    "country":"COUNTRY",
+    "target":"TARGET",
+    "address":"ADDRESS",
+}
+var longDesc = {
+    "entity":"Business or company name, eg Bank of America, Pay Pal or Microsoft. This also includes acronyms of business or company names such as BoA.",
+    "link": "A URL, eg www.jdjsjds.com",
+    "id": "A number such as shipping number or Identification number. Eg 326763-38 or PK-3782629",
+    "date":"A date eg, 25/01/2022 or 25th of January 2022",
+    "year":"a year, eg 2022. Do not include the year tag if it is part of a date.",
+    "person":"A name of the person that is NOT the target / recipient of the email. Eg. John Smith",
+    "country":"A country or continent. Eg Europe or Germany",
+    "target":"The name or email address of the target or recipient of the email. Eg. John Smith or john.smith@gmail.com",
+    "address":"An address or postal address. Eg. 20 Exhibition Walk, Clayton VIC 3168",
+}
+var shortcutKey = {
+    "entity": "e",
+    "link": "l",
+    "id": "i",
+    "date":"d",
+    "year":"y",
+    "person":"p",
+    "country":"c",
+    "target":"t",
+    "address":"a",
+}
+
